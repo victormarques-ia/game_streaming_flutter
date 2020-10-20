778a7362b0d6e9455864938afc184d1a318bf071
Victor Silva <victormarques.ia@gmail.com>
2020-10-20 08:55:51 -0300

    feat: create Explore screen

--- lib/screens/explore/explore.dart
+++ lib/screens/explore/explore.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:game_streaming/utils/constants.dart';
+import 'package:game_streaming/screens/explore/explore_all.dart';
+
 class Explore extends StatefulWidget {
   @override
   _ExploreState createState() => _ExploreState();
 }
 
 class _ExploreState extends State<Explore> {
+  Widget currentWidgetView;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      currentWidgetView = ExploreAll();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          "Explore",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
+    return Scaffold(
+      backgroundColor: kSecondaryColor,
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 250),
+        child: currentWidgetView,
       ),
     );
   }
